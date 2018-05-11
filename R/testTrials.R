6c6f75bc49cba46ef07ac4f8d9236c9c5b6268a7
Chad Ross <me@chadross.org>
2018-05-11 13:17:58 -0500

    populate readme

--- R/testTrials.R
+++ R/testTrials.R
@@ -1,27 +1,6 @@
 testTrials <- function(p, observations = 20, fail.criteria = 1,
                        raw.data = FALSE, data.structure = "data.frame") {
     # Completes a simulated drop test.
-    #
-    # Drop testing -- performed by the military and NASA -- yields results
-    # that are difficult to analyze. Numerous tech briefs and standards have
-    # attempted to address the problem. While fundamentally a binomial 
-    # process, drop testing almost always produces truncated data. Testing
-    # stops as soon as a failure condition is reached. If the failure
-    # condition occurs on drop one or two -- depending on the failure 
-    # criteria -- then the test returns only one or two result values.
-    # Alternatively, if the failure condition occurs on the last observation,
-    # then the test returns as many result values as observations.
-    #
-    # Drop testing is used to evaluate if a material will interact with liquid
-    # (LOX) or gaseous oxygen (GOX). The material is exposed to the LOX or GOX
-    # and an impactor is dropped onto the sample. Each drop is a bernoulli
-    # trial where a reaction is a failure and a non-reaction is a success. The
-    # specified number of trials -- until failure -- produces one test run.
-    #
-    # Simulation can be used to examine the behavior of this truncated test
-    # procedure.
-    #
-    # For more information on drop testing: 
     # 
     # Args:
     #   p: Integer. Specifies the probability that a reaction occurs.
