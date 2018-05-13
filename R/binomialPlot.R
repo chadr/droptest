7a60749923a60ec05f587d34cf1c81c1ace0dc15
Chad Ross <me@chadross.org>
2018-05-12 21:01:03 -0500

    update plot

--- R/binomialPlot.R
+++ R/binomialPlot.R
@@ -1,24 +1,40 @@
 binomialplot <- function(...) {
   arguments <- list(...)
-  #print(arguments)
-  p.range <- seq(0.01, 0.5, by=0.01)
-  # requires: max.trials, num.series, probs
+  
+  # values of P for simulated data
+  p.range <- seq(0.01, 0.5, by=0.05)
+
+  # values of P for binomial distribution curve
+  b.range <- seq(0.01, 0.5, by=0.01)
+  
+  # generate groups of simulated data
   obs.group <- groups(multi.p = TRUE, probs = p.range, ...)
-  #aggregate by p and get sd of $reactions
+  
+  #aggregate by P and get std deviation of reactions
   obs.group$PCT_REACT_SD <- obs.group$PCT_REACT
   obs.group <- aggregate(PCT_REACT_SD ~ P, FUN = sd, data = obs.group)
-  #print(obs.group)
   
-  q <- 1 - p.range
+  # generate binomial distribution data
+  q <- 1 - b.range
   n <- arguments$max.trials
-  binomial.group <- data.frame(BIN_P = p.range, BIN_SD = sqrt((p.range * q) / n))
+  binomial.group <- data.frame(BIN_P = b.range, 
+  	                           BIN_SD = sqrt((b.range * q) / n))
   
   old <- data.frame(P = c(0.01, 0.02, 0.03, 0.04, 0.05, 0.0656, 0.0908, 0.1562, 0.256,0.4),
                     SD = c(.022, .027, 0.034, 0.045, 0.047, 0.060, 0.066, 0.079, 0.085, 0.125))
-  smoothingSpline = smooth.spline(obs.group$P, obs.group$PCT_REACT_SD, spar=0.35)
   
-  plot(obs.group$P, obs.group$PCT_REACT_SD, ylim = c(0, .20))
+  # smooth curve for simulated std dev points
+  smoothingSpline = smooth.spline(obs.group$P, obs.group$PCT_REACT_SD,
+                                  spar=0.35)
+  
+  # create binomial plot
+  # plot simulated std dev points
+  plot(obs.group$P, obs.group$PCT_REACT_SD, pch = 19, ylim = c(0, 0.18),
+       xlim=c(0, 0.5))
+  # add smoothed curve for simulated std dev points
   lines(smoothingSpline, col = "blue", lwd=2)
+  # add binomial distribution curve
   lines(binomial.group$BIN_P, binomial.group$BIN_SD, col = "red", lwd = 2,)
-  points(old$P, old$SD, col = "red")
+  # add data points used in original paper
+  points(old$P, old$SD, col = "black", pch=15)
 }
