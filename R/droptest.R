b5d48771bd00f5f8d83cacc26981ff2fab3c7dd2
Chad Ross <me@chadross.org>
2018-05-18 14:23:27 -0500

    update documentation

--- R/droptest.R
+++ R/droptest.R
@@ -32,56 +32,67 @@
 #' \strong{Note:} This work is not endorsed by or affiliated with NASA. Released
 #' under MIT license.
 #' 
-#' \strong{Definitions}
-#'  Trial: A simulated bernoulli trial that represents one drop of the impactor
-#'  onto a material sample. Hence a trial is also referred to as a drop. Where q
-#'  is the probability of a failure. A reaction is recorded as a failure. Where
-#'  p is the probability of success. A non-reaction is recorded as a success.
-#'  Where \eqn{p = 1 - q}. See \url{https://en.wikipedia.org/wiki/Bernoulli_trial}
+#' @definitions
 #' 
-#'  Drop Test: A collection of simulated trials (drops) generated with equal
-#'  parameters (q, number of trials, failure criteria, etc). When the failure
-#'  criteria is reached the test is immediately terminated and no more trials
-#'  are completed. The sooner a test reaches the failure criteria results in
-#'  less total trials for that particular test. A test that consists of no
+#' \itemize{
+#'  \item Trial: A simulated bernoulli trial that represents one drop of the
+#'  impactor onto a material sample. Hence a trial is also referred to as a
+#'  drop. Where q is the probability of a failure. A reaction is recorded as a
+#'  failure. Where p is the probability of success. A non-reaction is recorded
+#'  as a success. Where \eqn{p = 1 - q}.
+#'  See \url{https://en.wikipedia.org/wiki/Bernoulli_trial}
+#' 
+#'  \item Drop Test: A collection of simulated trials (drops) generated with
+#'  equal parameters (q, number of trials, failure criteria, etc). When the
+#'  failure criteria is reached the test is immediately terminated and no more
+#'  trials are completed. The sooner a test reaches the failure criteria results
+#'  in less total trials for that particular test. A test that consists of no
 #'  failures will always contain the maximum nuber of trials as defined in the
 #'  function parameters.
 #' 
-#'  Test Series: A collection of simulated drop tests. A group number can be
-#'  attached to the drop tests in a test series (optional).
+#'  \item Test Series: A collection of simulated drop tests. A group number can
+#'  be attached to the drop tests in a test series (optional).
 #' 
-#'  Groups: A collection of multiple simulated test series. Each batch of test
-#'  series are identified with a group number. Within each group test parameters
-#'  will be identical.
+#'  \item Groups: A collection of multiple simulated test series. Each batch of
+#'  test series are identified with a group number. Within each group test
+#'  parameters will be identical.
 #' 
-#'  Trial Deviation: The average distance from q for the total percent of
+#'  \item Trial Deviation: The average distance from q for the total percent of
 #'  reactions (failures).
+#' }
 #' 
 #' @references
-#' Pass/Fail criteria and number of observations required have been defined in
-#' the following standards:
-#'  
-#' NASA-STD-6001B
-#' \url{https://spaceflightsystems.grc.nasa.gov/SpaceDOC_II/Standards/documents/NASA-STD-6001B-1.pdf}
-#' ASTM D2512 \url{https://www.astm.org/Standards/D2512.htm}
-#' ASTM G86-17 \url{https://www.astm.org/Standards/G86.htm}
-#'
+#'  Pass/Fail criteria and number of observations required have been defined in
+#'  the following standards:
+#' \itemize{
+#'  \item NASA-STD-6001B
+#'  \url{https://spaceflightsystems.grc.nasa.gov/SpaceDOC_II/Standards/documents/NASA-STD-6001B-1.pdf}
+#' 
+#'  \item ASTM D2512 \url{https://www.astm.org/Standards/D2512.htm}
+#' 
+#'  \item ASTM G86-17 \url{https://www.astm.org/Standards/G86.htm}
+#' }
+#' 
 #' \strong{Note:} This package is not constrained by any standard. Arbitrary
 #' test criteria and observations can be specified for maximum flexibility.
 #' 
 #' For more information on drop testing: 
 #' 
-#'  "An Instrument for Determination of Impact Sensitivity of Materials in 
+#' \itemize{
+#'  \item "An Instrument for Determination of Impact Sensitivity of Materials in 
 #'  Contact with Liquid Oxygen" (AB6002-EB). Lucas (1960). 
 #'  \url{https://www.astm.org/DIGITAL_LIBRARY/STP/MMR/PAGES/AB6002-EB.htm}
 #'
-#'  "Reproducibility of Liquid Oxygen Impact Test Results" (NASA-TN-D-7905). 
-#'  Gayle (1970).
+#'  \item "Reproducibility of Liquid Oxygen Impact Test Results"
+#'  (NASA-TN-D-7905). Gayle (1970).
 #'  \url{https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19750014413.pdf}
 #'
-#'  "Lox/Gox Mechanical Impact Tester Assessment" (TM-74106). Bransford, Bryan,
-#'  Frye, Stohler (1980).
+#'  \item "Lox/Gox Mechanical Impact Tester Assessment" (TM-74106). Bransford,
+#'  Bryan, Frye, Stohler (1980).
 #'  \url{https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19800006920.pdf}
+#' }
+#' 
+#' @author Chad Ross \email{chad.ross@gmail.com}
 #' 
 #' @seealso 
 #'  \code{\link{dtrials}}
@@ -95,8 +106,6 @@
 #'  \url{https://github.com/chadr/droptest}
 #'
 #' @docType package
-#' 
-#' @author Chad Ross \email{chad.ross@gmail.com}
 #' 
 #' @name droptest
 NULL
